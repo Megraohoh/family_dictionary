b839c029aadf269ab13226050e4cd7379b5dee42
Meghan Debity <mdebity@gmail.com>
2018-07-16 10:06:57 -0500

    updated comprehension

--- family_dict.py
+++ family_dict.py
@@ -9,8 +9,17 @@ my_family = {
         'age': 24
     }
 }
-family_siblings = set()
-for family_member, member_values in my_family.items():
-    family_siblings.add(f'{member_values["name"]} is my {family_member} and is {str(member_values["age"])} years old' )
+# How to set with a loop:
+# family_siblings = set()
+# for family_member, member_values in my_family.items():
+#     family_siblings.add(f'{member_values["name"]} is my {family_member} and is {str(member_values["age"])} years old' )
 
-print("My family!", family_siblings)
+# print("My family!", family_siblings)
+
+# How to set with comprehension:
+family_stuff = {
+    f'{member_values["name"]} is my {family_member} and is {str(member_values["age"])} years old.'
+    for (family_member, member_values) in my_family.items()
+}
+
+print("My family!", family_stuff)
